2a1a8a444e44b04166f6cf8acd38a671f5b1a2c2
Cristhian <cristhiangomezdev@gmail.com>
2023-03-11 11:38:26 +0100

    second

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,21 @@
+import ThemeProvider, { useBootstrapBreakpoints } from 'react-bootstrap/esm/ThemeProvider'
 import { Link } from 'react-router-dom'
 import rickmor from '../Assets/img/rick-morty.png'
 import Button from '../Components/Button/Button'
 import './Home.css'
 
+
 const Home = () => {
   return (
-    <div className='container'>
+
+    <div className='container sm'>
+<ThemeProvider>
         <h1 className='titlehome'>Rick & Morty</h1>
       <img className='imghome' src={rickmor}></img>
       <Link to="/personajes">
       <Button clase='ver' nombre='Buscar personaje'></Button>
       </Link> 
+      </ThemeProvider>
     </div>
   )
 }
